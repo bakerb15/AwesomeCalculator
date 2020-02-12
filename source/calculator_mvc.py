f43ec00807358e0318fc1e786a42dfbac483f067
bakerb15 <bradley.j.baker@outlook.com>
2020-02-12 18:59:08 -0500

    Custom buttons and Comments
    Added some comments and demostrated how a custom button could be added

--- source/calculator_mvc.py
+++ source/calculator_mvc.py
@@ -2,6 +2,11 @@ import PySimpleGUI as sg
 
 
 class CalculatorView(object):
+    """
+        Objects of this class keep track of
+        the theme and layout that are passed to
+        PysimpleGui Window.
+    """
 
     def __init__(self, *args, **kwargs):
         self.theme = 'DarkTanBlue'
@@ -13,13 +18,23 @@ class CalculatorView(object):
                 [sg.Button('0'), sg.Button('+'), sg.Button('-')],
                 [sg.Button('*'), sg.Button('/'), sg.Button('.')],
                 [sg.Button('('), sg.Button(')'), sg.Button('Clear')],
+                [],
                 [sg.Button('='), sg.Button('Cancel')]]
+        self.custom_row = self.layout[7] # reference to the row in the layout that can handle more buttons
 
 
 class CalculatorModel(object):
+    """ the user's arithmetic expression
+    will be stored as a string in the calculation member 
+    If a valid expression is executed the result
+    will overwrite the calculations string.
+
+    history will contain a list of tuples containing
+    expression, result
+    """
 
     def __init__(self, *args, **kwargs):
-        self.calculation = ""
+        self.calculation = "" 
         self.history = []
 
     def add_to_history(self, formula, result):
@@ -29,24 +44,37 @@ class CalculatorModel(object):
 class CalculatorController(object):
 
     def __init__(self, *args, **kwargs):
-        pass
+        self.stored_precedures = {} # Maps an event to a function 
 
     def process(self, window, model):
+        """ Catches button clicks from the window.
+        If the user presses the '=' this method
+        will attempt to use python's eval method
+        to execute the expression dispalyed in the
+        calculators text box. If the calculation
+        is invalid eval will throw an exception,
+        which results in an error message being 
+        displayed to the user.
+        """
         event, values = window.read()
-        if event in (None, 'Cancel'):	# if user closes window or clicks cancel
+        if event in (None, 'Cancel'):	# closing window or clicking cancel returns False
             return False
         calc_input =  window['_CALCINPUT_']
         model.calculation = values['_CALCINPUT_']
-        if event == '=':
-            try:
-                result = eval(model.calculation)
-                model.add_to_history(model.calculation, result)
-                model.calculation = result
-            except:
-                model.calculation = 'ERROR :('
-        elif event == 'Clear':
-            model.calculation = ''
+        if event in self.stored_precedures:
+            self.stored_precedures[event](model)
+            calc_input.update(value=model.calculation)
         else:
-            model.calculation = values['_CALCINPUT_'] + event
-        calc_input.update(value=model.calculation)
-        return True
+            if event == '=':
+                try:
+                    result = eval(model.calculation)
+                    model.add_to_history(model.calculation, result)
+                    model.calculation = result
+                except:
+                    model.calculation = 'ERROR :('
+            elif event == 'Clear':
+                model.calculation = ''
+            else:
+                model.calculation = values['_CALCINPUT_'] + event
+            calc_input.update(value=model.calculation)
+        return True # The model and view have been updated
--- source/main.py
+++ source/main.py
@@ -1,13 +1,23 @@
 from calculator_mvc import CalculatorView, CalculatorController, CalculatorModel
 import PySimpleGUI as sg
+import math
+
+def two_pi(model):
+    model.calculation += str(2*math.pi)
 
 def main():
+    
     model = CalculatorModel()
     view = CalculatorView()
     controller = CalculatorController()
+
+    #add a custom button with a stored procedure
+    tp = '2pi'
+    view.custom_row.append(sg.Button(tp))
+    controller.stored_precedures[tp] = two_pi
+
     sg.theme(view.theme)
     window = sg.Window('Awesome Calculator', view.layout)
-    # Event Loop to process "events" and get the "values" of the inputs
     while True:
         if controller.process(window, model) is False: # the user clicked the cancel button
             break
